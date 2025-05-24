f2eab6e1ca6035eddbcb564f1e1b7281fb386c74
wjkonger <wjkonger@gmail.com>
2025-05-24 02:20:03 -0400

    Template Driven Form

--- src/app/custom.pipe.ts
+++ src/app/custom.pipe.ts
@@ -1,19 +1,21 @@
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'custom'
+  name: 'custom',
+  pure: false
 })
 export class CustomPipe implements PipeTransform {
   str: string = "";
 
-
+  
   transform(value: unknown, ...args: unknown[]): unknown {
+
     if(value == '' || value == null || value == undefined)
       return 'N/A'
     else
     {
       this.str = value as string
-
       
       const areaCodeStr = this.str.substring(0,3);;
       const midSectionStr = this.str.substring(3, 6);
