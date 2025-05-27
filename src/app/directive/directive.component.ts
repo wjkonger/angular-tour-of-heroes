59a55c71cda5ae3ce2261fe410ac1c8267b258d1
wjkonger <wjkonger@gmail.com>
2025-05-27 10:06:51 -0400

    Lazy loading

--- src/app/directive/directive.component.ts
+++ src/app/directive/directive.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directive',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './directive.component.html',
   styleUrl: './directive.component.css'
 })
@@ -20,4 +21,27 @@ isChecked: boolean = false;
     {name: "han", email: "han@gmail.com"},
     {name: "shun", email: "shun@gmail.com"}
   ]
+
+  textColor: string = 'text-success';
+  changeColor(color: string) {
+    this.textColor = color;
+  }
+
+  styleColor: string = "orange";
+  updateColor(ngStyleColor: string) {
+    this.styleColor = ngStyleColor;
+  }
+  // 2
+  isTextGreen: boolean = false;
+
+  isTextOrange: boolean = false;
+  // 3
+  userClass: string = '';
+
+  customClass : any = {
+    'color':'yellow',
+    'height':'150px',
+    'width':'400px',
+    'border':'1px solid black'
+  }
 }
